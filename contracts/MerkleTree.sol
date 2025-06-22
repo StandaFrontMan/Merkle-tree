826773ef1f9826add8f1d7f0833590fdbc0775e6
Stanislav Vin <vinninjaq@gmail.com>
2025-06-22 23:49:57 +0200

    feature: keccak256 impl, tests

--- contracts/MerkleTree.sol
+++ contracts/MerkleTree.sol
@@ -5,5 +5,24 @@ pragma solidity ^0.8.28;
 // import "hardhat/console.sol";
 
 contract MerkleTree {
-  
+  // H1 H2 H3 H4
+  // TX1 TX2 TX3 TX4
+  bytes32[] public hashes;
+  string[4] transactions = [
+    "TX1: Maria => Alice",
+    "TX2: Alice => Shara",
+    "TX3: Alice => Elisabeth",
+    "TX4: Elisabeth => Jovana"
+  ];
+
+  function abiEncode(string memory input) public pure returns(bytes memory) {
+    return abi.encodePacked(input);
+  }
+
+  function makeHash(string memory input) public pure returns(bytes32) {
+    return keccak256(
+      abiEncode(input)
+    );
+  }
+
 }
---
+++ test/MerkleTree.test.ts
@@ -0,0 +1,46 @@
+import chai, { expect, should } from "chai";
+import { ethers } from "hardhat";
+import { Signer } from "ethers";
+
+import { MerkleTree } from "../typechain-types";
+
+describe("MerkleTree", function () {
+  let owner: Signer;
+  let ct: MerkleTree;
+
+  const transactions: string[] = [
+    "TX1: Maria => Alice",
+    "TX2: Alice => Shara",
+    "TX3: Alice => Elisabeth",
+    "TX4: Elisabeth => Jovana",
+  ];
+
+  beforeEach(async function () {
+    [owner] = await ethers.getSigners();
+
+    const MerkleTree = await ethers.getContractFactory("MerkleTree", owner);
+    ct = await MerkleTree.deploy();
+    await ct.waitForDeployment();
+  });
+
+  describe("Right way of hashing", async () => {
+    it("abiEncode func", async () => {
+      const result = await ct.abiEncode(transactions[0]);
+
+      const mark = ethers.solidityPacked(["string"], [transactions[0]]);
+
+      expect(result).to.be.equal(mark);
+    });
+
+    it("makeHash func", async () => {
+      const result = await ct.makeHash(transactions[0]);
+
+      const mark = ethers.solidityPackedKeccak256(
+        ["string"],
+        [transactions[0]]
+      );
+
+      expect(result).to.be.equal(mark);
+    });
+  });
+});
